9ab7af010683af5d70ade357c76496276dbd9389
Sumit Sahu <sssahu1009@gmail.com>
2023-07-11 23:47:23 +0530

    fixing the bug in header

--- js/main.js
+++ js/main.js
@@ -6,19 +6,10 @@ window.onscroll = function() {
         nav.classList.remove("header-scrolled");
     }
 }
-  
 let navBar = document.querySelectorAll(".nav-link");
-// let navCollapse = document.querySelector(".offcanvas-end.offcanvas");
-// navBar.forEach(function(a){
-//     a.addEventListener("click", function(){
-//         navCollapse.classList.remove("show");
-//     })
-// })
-
-let navCollapse = document.querySelector(".offcanvas-end.offcanvas");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
 navBar.forEach(function(a) {
   a.addEventListener("click", function() {
-    navCollapse.style.visibility = "hidden";
     navCollapse.classList.remove("show");
   });
 });
